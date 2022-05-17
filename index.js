673fa2f5803632ec423103d814291b431049602f
Thai Xiong <thai303@hotmail.com>
2022-05-17 10:11:49 -0600

    commit tree d9f71af46882a28689abe77d61192bdfbb1cb115

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(a,b) {
+    return a/b
+    console.log(divide(a,b));
+}
+function square(no){
+    return no ** 2
+}
+
+let squared = square(5);
+console.log(" 2 ** 2 = ",squared)
+
+let add = function(a,b) {
+    return a + b
+    console.log(add(a,b))
+}
